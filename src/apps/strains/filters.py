7f1b5cb80416cb70e17b25b2c16b2a60a565f7a6
Stanislav Khoshov <khoshov@gmail.com>
2020-02-09 15:29:14 +0300

    add strain search

--- src/apps/strains/filters.py
+++ src/apps/strains/filters.py
@@ -9,7 +9,13 @@ class StrainFilter(django_filters.FilterSet):
     height = django_filters.MultipleChoiceFilter(choices=Strain.HEIGHT)
     crop = django_filters.MultipleChoiceFilter(choices=Strain.CROP)
     flowering = django_filters.MultipleChoiceFilter(choices=Strain.FLOWERING)
+    search = django_filters.CharFilter(field_name="name", method="search_filter")
 
     class Meta:
         model = Strain
-        fields = ['type', 'thc', 'difficulty', 'height', 'crop', 'flowering', ]
+        fields = ['type', 'thc', 'difficulty', 'height', 'crop', 'flowering', 'search']
+
+    # noinspection PyMethodMayBeStatic
+    def search_filter(self, queryset, name, value):
+        lookup = '__'.join([name, 'icontains'])
+        return queryset.filter(**{lookup: value})
