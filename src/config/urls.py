579bdb492243442945a3b0fbef2025b8433f1a5e
Stanislav Khoshov <khoshov@gmail.com>
2020-04-11 23:25:34 +0300

    update urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 
-from config import settings
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
